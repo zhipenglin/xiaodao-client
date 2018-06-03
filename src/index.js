8d80a35890490de3ad464554b2c241b6dac5023e
linzhipeng <zhipeng.lin@ifchange.com>
2018-06-03 15:51:01 +0800

    xxxxx

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'amfe-flexible';
+import axios from 'axios';
 import initReactFastclick from 'react-fastclick';
 initReactFastclick();
+
+axios.interceptors.request.use((config)=>{
+    config.url=`/api${config.url}`;
+    return config;
+});
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
